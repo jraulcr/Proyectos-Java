9a049979f9cd5ec1a09419c2559a40416efff1be
jraulcr <jraulcr@gmail.com>
2021-01-28 18:11:16 +0100

    Continuación curso Spring + Hibernate 27-01-2021

---
+++ ProyectoHibernate/src/es/pildoras/pruebasHibernate/PruebasJDBC.java
@@ -0,0 +1,26 @@
+package es.pildoras.pruebasHibernate;
+
+import java.sql.DriverManager;
+import java.sql.Connection;
+
+public class PruebasJDBC {
+
+	public static void main(String[] args) {
+
+		String jdbcUrl = "jdbc:mysql://localhost:3306/pruebasHibernate?useSSL=false";
+		String usuario = "root";
+		String contra = "root1234";
+
+		try {
+
+			System.out.println("Intentando conectar con la BB.DD" + jdbcUrl);
+			Connection miConexion = DriverManager.getConnection(jdbcUrl, usuario, contra);
+			System.out.println("Conexión exitosa");
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
